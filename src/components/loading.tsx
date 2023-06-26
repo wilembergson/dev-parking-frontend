3d7eb5c713ae27e569cdd3006343885eb0aba059
wilembergson <wilembergson@yahoo.com>
2023-06-26 01:21:00 -0300

    register user implemented

---
+++ src/components/loading.tsx
@@ -0,0 +1,9 @@
+import { Ring } from "react-cssfx-loading"
+
+export default function Loading() {
+    return (
+        <div className="my-56">
+            <Ring color="#FFBF00" width="150px" height="150px" />
+        </div>
+    )
+}
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -3,8 +3,8 @@ import Input from "./input";
 import FormButtom from "./form-butom";
 import { useState } from "react";
 import api from "@/api/api-connections";
-import { errorToast } from "@/utils/toasts";
-import { ToastContainer } from "react-toastify";
+import { errorToast, sucessToast } from "@/utils/toasts";
+import Loading from "./loading";
 
 export default function SignupForm({ setForm }: any) {
     const initialData: Signup = {
@@ -26,11 +26,8 @@ export default function SignupForm({ setForm }: any) {
         const user = { ...formData }
         try {
             const res = await api.signup(user)
-            alert(res.data)
-            console.log(res.data)
-            //setForm('login')
-            //sucessMessage(data.message)
-            //setTimeout(() => navigation('/'), 3000)
+            sucessToast('Usuário criado com sucesso!')
+            setForm('login')
         } catch (error: any) {
             setLoading(false)
             const errorMessage = (
@@ -44,48 +41,50 @@ export default function SignupForm({ setForm }: any) {
 
     return (
         <div className="flex relative flex-col bg-slate-100 xl:w-6/12 w-full  py-16 rounded-3xl items-center" data-aos="flip-left">
+            {(loading === false) ?
+                <>
+                    <Title textColor="text-gray-500" textSize="text-4xl" textBlack>
+                        Novo usuário
+                    </Title>
+                    <form onSubmit={handleSubmit} className="flex flex-col items-center w-full mt-10 font-principal">
+                        <Input
+                            placehoder="nome"
+                            type="text"
+                            onChange={(e: any) => handleChange(e)}
+                            name="name"
+                            value={formData.name}
+                        />
+                        <Input
+                            placehoder="RG"
+                            type="number"
+                            onChange={(e: any) => handleChange(e)}
+                            name="rg"
+                            value={formData.rg}
+                        />
+                        <Input
+                            placehoder="email"
+                            type="email"
+                            onChange={(e: any) => handleChange(e)}
+                            name="email"
+                            value={formData.email}
+                        />
+                        <Input
+                            type="password"
+                            placehoder="senha"
+                            onChange={(e: any) => handleChange(e)}
+                            name="password"
+                            value={formData.password}
+                        />
+                        <h3 onClick={() => setForm('login')} className="flex mt-10 text-gray-500 font-black cursor-pointer hover:text-yellow duration-500">
+                            Fazer login
+                        </h3>
+                        <FormButtom>
+                            Registrar
+                        </FormButtom>
 
-
-            <Title textColor="text-gray-500" textSize="text-4xl" textBlack>
-                Novo usuário
-            </Title>
-            <form onSubmit={handleSubmit} className="flex flex-col items-center w-full mt-10 font-principal">
-                <Input
-                    placehoder="nome"
-                    type="text"
-                    onChange={(e: any) => handleChange(e)}
-                    name="name"
-                    value={formData.name}
-                />
-                <Input
-                    placehoder="RG"
-                    type="number"
-                    onChange={(e: any) => handleChange(e)}
-                    name="rg"
-                    value={formData.rg}
-                />
-                <Input
-                    placehoder="email"
-                    type="email"
-                    onChange={(e: any) => handleChange(e)}
-                    name="email"
-                    value={formData.email}
-                />
-                <Input
-                    type="password"
-                    placehoder="senha"
-                    onChange={(e: any) => handleChange(e)}
-                    name="password"
-                    value={formData.password}
-                />
-                <h3 onClick={() => setForm('login')} className="flex mt-10 text-gray-500 font-black cursor-pointer hover:text-yellow duration-500">
-                    Fazer login
-                </h3>
-                <FormButtom>
-                    Registrar
-                </FormButtom>
-
-            </form>
+                    </form>
+                </> : <Loading />
+            }
         </div>
     )
 }
