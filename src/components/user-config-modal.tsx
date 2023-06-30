e41cffb88e5c4d32a0fc7b8d21ae3b0d602c611a
wilembergson <wilembergson@yahoo.com>
2023-06-30 02:07:13 -0300

    update inputs checkbox

--- src/components/user-config-modal.tsx
+++ src/components/user-config-modal.tsx
@@ -5,6 +5,9 @@ import { useGlobalContext } from "@/app/contexts/user";
 import api from "@/api/api-connections";
 import { errorToast } from "@/utils/toasts";
 import { BsCheckAll } from 'react-icons/bs'
+import Input from "./input";
+import { TfiEmail } from "react-icons/tfi";
+import { AiFillLock } from "react-icons/ai";
 
 type Props = {
     isVisible: boolean
@@ -16,6 +19,10 @@ type UserData = {
     rg: number,
     email: string
 }
+type Update = {
+    email?: string;
+    password?: string;
+}
 
 export default function UserConfigModal({ isVisible, onClick }: Props) {
     const { userId, userName } = useGlobalContext()
@@ -25,6 +32,17 @@ export default function UserConfigModal({ isVisible, onClick }: Props) {
         rg: 0,
         email: ''
     })
+    const [updateData, setUpdateData] = useState<Update>({})
+
+    const [isDisabledEmail, setIsDisabledEmail] = useState(false);
+    const handleCheckboxChangeEmail = () => {
+        setIsDisabledEmail(!isDisabledEmail);
+    };
+
+    const [isDisabledPassword, setIsDisabledPassword] = useState(false);
+    const handleCheckboxChangePassword = () => {
+        setIsDisabledPassword(!isDisabledPassword);
+    };
 
     async function loadUserData() {
         try {
@@ -45,6 +63,17 @@ export default function UserConfigModal({ isVisible, onClick }: Props) {
         }
     }
 
+    function handleChange({ target }: any) {
+        setUpdateData({ ...updateData, [target.name]: target.value })
+    }
+
+    function ok(){
+        setUpdateData({})
+        setIsDisabledEmail(false)
+        setIsDisabledPassword(false)
+        onClick(false)
+    }
+
     useEffect(() => {
         loadUserData()
     })
@@ -52,25 +81,70 @@ export default function UserConfigModal({ isVisible, onClick }: Props) {
     return (
         <Modal isVisible={isVisible}>
             {!loading ?
-                <div onClick={() => null} className="flex flex-col w-1/2 shadow-lg justify-center overflow-hidden items-center bg-white rounded-2xl" data-aos="zoom-in">
+                <div onClick={() => null} className="flex font-principal  flex-col w-1/2 shadow-lg justify-center overflow-hidden items-center bg-white rounded-2xl" data-aos="zoom-in">
                     <h1 className="font-principal font-black text-3xl flex-wrap w-full p-3 text-center bg-purple-500 text-white">
                         Informações do usuário
                     </h1>
-                    <section className="flex flex-col p-2">
-                        <h2 className="text-xl text-gray-clear font-black my-2">
-                            NOME: {userData.name}
-                        </h2>
-                        <h2 className="text-xl text-gray-clear font-black my-2">
-                            RG: {userData.rg}
-                        </h2>
-                        <h2 className="text-xl text-gray-clear font-black my-2">
-                            EMAIL: {userData.email}
-                        </h2>
-                    </section>
+                    <div className="flex w-full pt-4 px-4">
+                        <section className="flex flex-col w-1/2 p-2 border border-r-2 border-l-0 border-y-0 border-purple-200">
+                            <h2 className="text-xl text-gray-clear font-black my-2">
+                                <span className="text-purple-500">NOME:</span> {userData.name}
+                            </h2>
+                            <h2 className="text-xl text-gray-clear font-black my-2">
+                                <span className="text-purple-500 ">RG:</span> {userData.rg}
+                            </h2>
+                            <h2 className="text-xl text-gray-clear font-black my-2">
+                                <span className="text-purple-500">EMAIL:</span> {userData.email}
+                            </h2>
+                        </section>
+                        <form className="flex flex-col items-center w-1/2 font-principal pb-4">
+                            <h2 className="text-xl text-gray-clear font-black mb-6">
+                                Atualizar dados
+                            </h2>
+                            <section className="flex items-center rounded-lg overflow-hidden pl-3 mb-8 bg-white w-4/5">
+                                <input
+                                    type="checkbox"
+                                    className="form-checkbox h-5 w-5 bg-purple-500 mr-2 cursor-pointer transition duration-150 ease-in-out"
+                                    checked={isDisabledEmail}
+                                    onChange={handleCheckboxChangeEmail}
+                                />
+                                <TfiEmail size={25} color={isDisabledEmail ? "#FFBF00" : "#848484"} />
+                                <input
+                                    className={`flex w-full border-none ml-2 rounded-lg focus:outline-none p-3 text-gray bg-blue-50
+                                    ${!isDisabledEmail ? 'pointer-events-none opacity-50' : ''}`}
+                                    placeholder="email"
+                                    type="email"
+                                    onChange={(e: any) => handleChange(e)}
+                                    name="email"
+                                    value={updateData!.email!}
+                                    required
+                                />
+                            </section>
+                            <section className="flex items-center rounded-lg overflow-hidden pl-3 bg-white w-4/5">
+                                <input
+                                    type="checkbox"
+                                    className="form-checkbox h-5 w-5 bg-purple-500 mr-2 cursor-pointer transition duration-150 ease-in-out"
+                                    checked={isDisabledPassword}
+                                    onChange={handleCheckboxChangePassword}
+                                />
+                                <AiFillLock size={25} color={isDisabledPassword ? "#FFBF00" : "#848484"} />
+                                <input
+                                    className={`flex w-full border-1 ml-2 rounded-lg focus:outline-none p-3 text-gray bg-blue-50
+                                    ${!isDisabledPassword ? 'pointer-events-none opacity-50' : ''}`}
+                                    type="password"
+                                    placeholder="senha"
+                                    onChange={(e: any) => handleChange(e)}
+                                    name="password"
+                                    value={updateData!.password}
+                                    required
+                                />
+                            </section>
+                        </form>
+                    </div>
                     <section className="flex w-3/4 justify-around">
                         <button className="flex items-center font-principal text-2xl my-6 text-white  bg-blue-500 px-4 py-2 rounded-lg hover:bg-blue-400 transition duration-500"
-                            onClick={() => onClick(false)}>
-                                <BsCheckAll size={32}/>OK
+                            onClick={() => ok()}>
+                            <BsCheckAll size={32} />OK
                         </button>
                     </section>
                 </div> : <Loading />}
