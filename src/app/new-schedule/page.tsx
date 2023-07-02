5ac530f77c43c6e0f939c685a6f926c7a1b76f6b
wilembergson <wilembergson@yahoo.com>
2023-07-01 22:12:51 -0300

    new schedule: client informations

--- src/app/new-schedule/page.tsx
+++ src/app/new-schedule/page.tsx
@@ -22,27 +22,44 @@ export default function NewSchedule() {
             <>
                 <Header showLogout={setShowLogout} />
                 <Main>
-                    <section className='flex justify-center items-center sm:w-3/5 w-full pt-4 pb-2 sm:px-0 px-2 border border-b-0 border-t-0 border-x-0 border-gray-clear'>
+                    <section className='flex justify-center font-principal items-center sm:w-3/5 w-full pt-4 pb-2 sm:px-0 px-2 border border-b-0 border-t-0 border-x-0 border-gray-clear'>
                         <h1 className='text-5xl text-gray-clear font-black mr-4 mt-4'>
                             Nova reserva
                         </h1>
                     </section>
-                    <section className='flex justify-start items-center sm:w-3/5 w-full pt-4 pb-2 sm:px-0 px-2 mt-6 border border-b-1 border-t-0 border-x-0 border-gray-clear'>
-                        <h1 className='text-2xl text-yellow font-black mr-4'>
-                            Dados da vaga
-                        </h1>
-                    </section>
-                    <section className='flex flex-col justify-start items-start sm:w-3/5 w-full pt-4 pb-2 sm:px-0 px-2 mt-2'>
-                        <h1 className='text-xl text-gray-clear font-black mr-4'>
-                        <span className="text-purple-500">Localização:</span> {vacancy?.localization}
-                        </h1>
-                        <h1 className='text-xl text-gray-clear font-black mr-4'>
-                        <span className="text-purple-500">Veículo:</span> {vacancy?.type === 'CAR' ? 'Carro' : 'Moto'}
-                        </h1>
-                        <h1 className='text-xl text-gray-clear font-black mr-4'>
-                        <span className="text-purple-500">Status:</span> {vacancy?.occupied ? 'Ocupada' : 'Disponível'}
-                        </h1>
-                    </section>
+                    <div className=" flex font-principal sm:w-3/5 w-full mt-4">
+                        <div className="flex flex-col w-2/5">
+                            <section className='flex justify-start items-center w-full pt-4 pb-2 sm:px-0 px-2 mt-6 border border-b-1 border-t-0 border-x-0 border-gray-clear'>
+                                <h1 className='text-2xl text-yellow font-black mr-4'>
+                                    Dados da vaga
+                                </h1>
+                            </section>
+                            <section className='flex flex-col justify-start items-start sm:w-3/5 w-full pt-4 pb-2 sm:px-0 px-2 mt-2'>
+                                <h1 className='text-xl text-gray-clear font-black mr-4'>
+                                    <span className="text-purple-500">Localização:</span> {vacancy?.localization}
+                                </h1>
+                                <h1 className='text-xl text-gray-clear font-black mr-4'>
+                                    <span className="text-purple-500">Veículo:</span> {vacancy?.type === 'CAR' ? 'Carro' : 'Moto'}
+                                </h1>
+                                <h1 className='text-xl text-gray-clear font-black mr-4'>
+                                    <span className="text-purple-500">Status:</span> {vacancy?.occupied ? 'Ocupada' : 'Disponível'}
+                                </h1>
+                            </section>
+                        </div>
+                        <div className="flex flex-col w-3/5">
+                            <section className='flex justify-center items-center w-full pt-4 pb-2 sm:px-0 px-2 mt-6 border border-b-1 border-t-0 border-x-0 border-gray-clear'>
+                                <h1 className='text-2xl text-yellow font-black mr-4'>
+                                    Informações do cliente
+                                </h1>
+                            </section>
+                            <section className='flex flex-col justify-center items-center w-full pt-4 pb-2 sm:px-0 px-2 mt-2'>
+                                <h1 className='text-xl text-gray-clear font-black mr-4'>
+                                    <span className="text-purple-500">RG: </span>
+                                </h1>
+                                <input className='flex bg-slate-200' type="number" />
+                            </section>
+                        </div>
+                    </div>
                 </Main>
             </>
             <LogoutModal isVisible={showLogout} onClick={() => setShowLogout(false)} />
